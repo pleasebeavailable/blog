a944addb277087e8188257d513763c3092ec0c70
Ante Sucur <ante_sucur@hotmail.com>
2019-10-30 11:28:30 +0100

    article form and hibernate db

--- src/main/java/com/example/blogpage/backend/repository/ArticleRepository.java
+++ src/main/java/com/example/blogpage/backend/repository/ArticleRepository.java
@@ -2,10 +2,12 @@ package com.example.blogpage.backend.repository;
 
 import com.example.blogpage.backend.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends CrudRepository<Article, Long> {
 
     Article findByName(String name);
+
 }
